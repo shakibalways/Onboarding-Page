aded795b3f67f9234f6e26698be3695aeb683e8d
S <shakibmohammad18@gmail.com>
2024-09-27 22:01:45 +0600

    change

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,7 @@
 import 'package:flutter/material.dart';
 import 'package:onboarding/screens/onboarding_screen.dart';
 
-void main() => runApp(MyApp());
+void main() => runApp(const MyApp());
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
