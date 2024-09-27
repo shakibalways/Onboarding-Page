568fa5b0f9d1f4c172ae167e68a6305c7843f185
S <shakibmohammad18@gmail.com>
2024-09-27 22:01:31 +0600

    change

--- lib/main.dart
+++ lib/main.dart
@@ -6,6 +6,8 @@ import 'package:onboarding/screens/onboarding_screen.dart';
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
