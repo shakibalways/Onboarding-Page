929b52ef97f547b7a9b44ee1597ec06890262e14
S <shakibmohammad18@gmail.com>
2024-09-27 22:03:49 +0600

    change

--- lib/main.dart
+++ lib/main.dart
@@ -2,24 +2,18 @@ import 'package:flutter/material.dart';
 import 'package:onboarding/screens/onboarding_screen.dart';
 
 void main() => runApp(
-
-      const
-      MyApp(),
+      const MyApp(),
     );
 
 class MyApp extends StatelessWidget {
-
   const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      title:
-      'Flutter Onboarding UI',
-      debugShowCheckedModeBanner:
-      false,
-      home:
-      OnboardingScreen(),
+      title: 'Flutter Onboarding UI',
+      debugShowCheckedModeBanner: false,
+      home: OnboardingScreen(),
     );
   }
 }
